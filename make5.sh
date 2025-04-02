9a1637e729a712ec99a70a0195b74e146fd62ab2
Christian Knoflach <christian@kno.at>
2025-04-02 10:59:55 +0200

    maptools

---
+++ make5.sh
@@ -0,0 +1,5 @@
+./map_maker.sh  10  10   5 > map_01.map
+./map_maker.sh  20  20  10 > map_02.map
+./map_maker.sh  30  30  10 > map_03.map
+./map_maker.sh  40  50  20 > map_04.map
+./map_maker.sh  80 400 100 > map_05.map
